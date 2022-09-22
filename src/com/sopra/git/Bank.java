9aa0373868ee1ab012d0fd51876dcc96ad052037
msravani <msravani@ITEM-S132884.emea.msad.sopra>
2022-09-22 15:53:37 +0530

    added deposit and withdraw methods

--- src/com/sopra/git/Bank.java
+++ src/com/sopra/git/Bank.java
@@ -4,14 +4,14 @@ public class Bank {
 	private int bankId;
 	private String bankName;
 	private String accNumber;
-	
+
 	public Bank(int bankId, String bankName, String accNumber) {
 		super();
 		this.bankId = bankId;
 		this.bankName = bankName;
 		this.accNumber = accNumber;
 	}
-	
+
 	public Bank() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -20,25 +20,30 @@ public class Bank {
 	public int getBankId() {
 		return bankId;
 	}
+
 	public void setBankId(int bankId) {
 		this.bankId = bankId;
 	}
+
 	public String getBankName() {
 		return bankName;
 	}
+
 	public void setBankName(String bankName) {
 		this.bankName = bankName;
 	}
+
 	public String getAccNumber() {
 		return accNumber;
 	}
+
 	public void setAccNumber(String accNumber) {
 		this.accNumber = accNumber;
 	}
+
 	@Override
 	public String toString() {
 		return "Bank [bankId=" + bankId + ", bankName=" + bankName + ", accNumber=" + accNumber + "]";
 	}
-	
 
 }
